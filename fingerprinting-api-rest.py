1f7ff471e50fef76a077b429b118a13085f9765d
vjgarciag96 <vjgarciag96@gmail.com>
2018-05-05 01:15:00 +0200

    API method refactored

--- fingerprinting-api-rest.py
+++ fingerprinting-api-rest.py
@@ -9,15 +9,18 @@ app = Flask(__name__)
 def add_fingerprint():
     return insert_fingerprint(request.json)
 
+def get_database():
+    client = MongoClient()
+    return client.rcmmDB
+
+def get_fingerprint_collection():
+    return get_database().fingerprint
 
 def insert_fingerprint(fingerprint):
-    client = MongoClient()
-    db = client.rcmmDB
-    insert_result = db.fingerprint.insert_one(fingerprint)
+    insert_result = get_fingerprint_collection().insert_one(fingerprint)
     result_dict = dict()
     result_dict['fingerprint_id'] = str(insert_result.inserted_id)
     return json.dumps(result_dict)
-
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=6969)
